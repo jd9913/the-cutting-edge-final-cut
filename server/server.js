817f381948dc0b4809b2ef237c51ca5686d422c9
Jennifer Schlierman <jd9913@gmail.com>
2021-02-28 17:09:29 -0700

    custom error handling

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
-import colors from "colors";
 
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
 	res.send("API is running");
