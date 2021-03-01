5227a00b59c1f7dcb41e1f139c57216c8e5de1b4
Jennifer Schlierman <jd9913@gmail.com>
2021-03-01 10:36:29 -0700

    adding redux

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,19 +1,14 @@
 import React, { useEffect } from "react";
-import { useQuery } from "@apollo/react-hooks";
+
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
-import { idbPromise } from "../../utils/helpers";
 import ProductItem from "../ProductItem";
-import { QUERY_PRODUCTS } from "../../utils/queries";
-import spinner from "../../assets/spinner.gif";
 
 function ProductList() {
 	const dispatch = useDispatch();
 	const state = useSelector((state) => state);
 
 	const { currentCategory } = state;
-
-	const { loading, data } = useQuery(QUERY_PRODUCTS);
 
 	useEffect(() => {
 		dispatch(listProducts());
@@ -36,7 +31,7 @@ function ProductList() {
 			</h3>
 			{state.products.length ? (
 				<div className='flex-row'>
-					{filterProcucts().map((product) => (
+					{filterProducts().map((product) => (
 						<ProductItem
 							key={product._id}
 							_id={product._id}
@@ -50,7 +45,6 @@ function ProductList() {
 			) : (
 				<h3>You haven't added any styles yet!</h3>
 			)}
-			{loading ? <img src={spinner} alt='loading' /> : null}
 		</div>
 	);
 }
